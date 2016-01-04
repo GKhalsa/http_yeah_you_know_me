dbe008d9b6270b651381a9430004035ab8940f15
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-01-04 15:08:09 -0700

    starting http yeah you know me

---
+++ lib/iter_0.rb
@@ -0,0 +1,10 @@
+require 'socket'
+
+tcp_server = TCPServer.new(9292)
+client = tcp_server.accept
+
+puts "Ready for a request"
+request_lines = []
+while line = client.gets and !line.chomp.empty?
+  request_lines << line.chomp
+end
