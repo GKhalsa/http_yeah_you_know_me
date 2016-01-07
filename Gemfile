1fcced4cfb8f8216cb8e17c761866539a56c8ec2
David Whitaker <damwhit@gmail.com>
2016-01-06 17:48:58 -0700

    made new ruby_server from scratch

--- Gemfile
+++ Gemfile
@@ -1,9 +1,8 @@
 source "https://www.rubygems.org"
 
 gem "mrspec"
-gem "minitest/autorun"
-gem "minitest/pride"
 gem "pry"
 gem "pry-nav"
 gem "simplecov"
 gem "hurley"
+gem "codeclimate-test-reporter", group: :test, require: nil
--- lib/ruby_server.rb
+++ lib/ruby_server.rb
@@ -1,56 +1,55 @@
+require 'socket'
+
 class Server
+    attr_reader :request_count
 
-  require 'socket'
-
-  tcp_server = TCPServer.new(9292)
-  server_count = 0
-
-  loop do
-
-    client = tcp_server.accept
-
-
-    # this is the request_lines array
-    puts "Ready for a request"
-    request_lines = ["Hello, World (#{server_count})"] #possible
-
-    while line = client.gets and !line.chomp.empty?
-      request_lines << line.chomp
-
-    end
-
-    server_count += 1
-    puts "Got this request:"
-    puts request_lines.inspect
-
-    puts "Sending response."
-    response = "<pre>" + request_lines.join("\n") + "</pre>"
-    output = "<html><head></head><body>#{response}</body></html>"
-    headers = ["http/1.1 200 ok",
-              "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-              "server: ruby",
-              "content-type: text/html; charset=iso-8859-1",
-              "content-length: #{output.length}\r\n\r\n"].join("\r\n")
-
-    client.puts headers
-    client.puts output
-
-    puts ["Wrote this response:", headers, output].join("\n")
-    puts "\nResponse complete."
-
-
-    client.close
-
-    puts "<pre>"
-    puts "Verb: #{request_lines[1].split[0]}"
-    puts "Path: #{request_lines[6].split[-1][21..-1]}"
-    puts "Protocol: #{request_lines[1].split[2]}"
-    puts "Host: #{request_lines[2].split[1][0..-6]}"
-    puts "Port: #{request_lines[2].split[1][-4..-1]}"
-    puts "Origin: #{request_lines[2].split[1][0..-6]}"
-    puts "Accept: #{request_lines[5].split[1]}"
-    puts "</pre>"
-
+  def initialize(port = 9292)
+    @server = TCPServer.new(port)
+    @request_count = 0
+    @request_lines = []
   end
 
+  def request
+    loop do
+      client = @server.accept    # Wait for a client to connect
+
+      while line = client.gets and !line.chomp.empty?
+        @request_lines << line.chomp
+      end
+      #
+      # # client.puts "Hello !"
+      # # client.puts "Time is #{Time.now}"
+      @request_count += 1
+      client.puts "Hello, World (#{@request_count/2})"
+      # client.puts "#{@request_lines}.inspect"
+      client.puts headers
+      client.puts output
+      client.close
+
+      @request_lines = []
+
+    end
+  end
+
+    def response
+      "<pre>" + @request_lines.join("\n") + "</pre>"
+    end
+
+    def output
+      "<html><head></head><body>#{response}</body></html>"
+    end
+
+    def headers
+      ["http/1.1 200 ok",
+      "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+      "server: ruby",
+      "content-type: text/html; charset=iso-8859-1",
+      "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+    end
+
 end
+
+server = Server.new
+
+server.request
+server.headers
